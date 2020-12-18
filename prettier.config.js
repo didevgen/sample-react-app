5bf0ea9069f63874ade397c3e812aa1e851f28ab
Eugene Kovaljov <ykova@softserveinc.com>
2020-12-18 17:59:01 +0200

    Add linters

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 120,
+    semi: true,
+    useTabs: false,
+    trailingComma: 'none',
+    singleQuote: true,
+    tabWidth: 4,
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+    endOfLine: 'lf'
+};
